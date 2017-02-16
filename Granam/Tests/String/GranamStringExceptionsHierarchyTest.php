ed260c06f6bc3a19f9b01bc6af26a82c42df86f2
Jaroslav Týc <mail@jaroslavtyc.com>
2017-02-16 17:06:42 +0100

    Fixed exceptions hierarchy tests

--- Granam/Tests/String/GranamStringExceptionsHierarchyTest.php
+++ Granam/Tests/String/GranamStringExceptionsHierarchyTest.php
@@ -1,24 +1,33 @@
 <?php
 namespace Granam\Tests\String;
 
-use Granam\Tests\Exceptions\Tools\AbstractExceptionsHierarchyTest;
+use Granam\Tests\ExceptionsHierarchy\Exceptions\AbstractExceptionsHierarchyTest;
 
-class ExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
+class GranamStringExceptionsHierarchyTest extends AbstractExceptionsHierarchyTest
 {
+    /**
+     * @return string
+     */
     protected function getTestedNamespace()
     {
         return $this->getRootNamespace();
     }
 
+    /**
+     * @return string
+     */
     protected function getRootNamespace()
     {
         return str_replace('\Tests', '', __NAMESPACE__);
     }
 
+    /**
+     * @return array|string[]
+     */
     protected function getExternalRootNamespaces()
     {
         return [
-            'Granam\Scalar'
+            'Granam\Scalar',
         ];
     }
 }
