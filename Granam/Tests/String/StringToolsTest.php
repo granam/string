b7ffebe7d97fa2dc7265ba11f57d4666f19ce86f
Jaroslav Týc <mail@jaroslavtyc.com>
2019-09-06 09:31:42 +0200

    Few more tests of camel case and snake case IDs

--- Granam/Tests/String/StringToolsTest.php
+++ Granam/Tests/String/StringToolsTest.php
@@ -179,7 +179,7 @@ class StringToolsTest extends TestCase
             ['Ι ι ιώτα', 'I i iota'], ['Κ κ κάππα', 'K k kappa'], ['Λ λ λάμδα', 'L l lamda'], ['Μ μ μυ', 'M m my'],
             ['Ν ν νυ', 'N n ny'], ['Ξ ξ ξι', 'X x xi'], ['Ο ο όμικρον', 'O o omikron'], ['Π π πι', 'P p pi'],
             ['Ρ ρ ρώ', 'R r ro'], ['Σ σ/ς σίγμα', 'S s/s sigma'], ['Τ τ ταυ', 'T t tau'], ['Υ υ ύψιλον', 'Y y ypsilon'],
-            ['Φ φ φι', 'PH ph phi'], ['Χ χ χι', 'CH ch chi'], ['Ψ ψ ψι', 'PS ps psi'], ['Ω ω ωμέγα', 'O o omega']
+            ['Φ φ φι', 'PH ph phi'], ['Χ χ χι', 'CH ch chi'], ['Ψ ψ ψι', 'PS ps psi'], ['Ω ω ωμέγα', 'O o omega'],
         ];
     }
 
@@ -430,6 +430,7 @@ class StringToolsTest extends TestCase
         self::assertSame('stringToolsTest', StringTools::toCamelCaseId(__CLASS__));
         self::assertSame('krevTeceVzdyckyCervena', StringTools::toCamelCaseId('Krev teče vždycky červená'));
         self::assertSame('vypocetPoctuOddelovacuZaSebou', StringTools::toCamelCaseId('Výpočet počtu oddělovačů za sebou'));
+        self::assertSame('flakonek02L', StringTools::toCamelCaseId('Flakónek (0.2 l)'));
     }
 
     /**
@@ -440,5 +441,6 @@ class StringToolsTest extends TestCase
         self::assertSame('string_tools_test', StringTools::toSnakeCaseId(__CLASS__));
         self::assertSame('krev_tece_vzdycky_cervena', StringTools::toSnakeCaseId('Krev teče vždycky červená'));
         self::assertSame('vypocet_poctu_oddelovacu_za_sebou', StringTools::toSnakeCaseId('Výpočet počtu oddělovačů za sebou'));
+        self::assertSame('flakonek_02l', StringTools::toSnakeCaseId('Flakónek (0.2 l)'));
     }
 }
